ff5b7f1db11ca52ed5eb802ae0403903d05b6b4b
Vishal Vasnani <vishalvasnani123@gmail.com>
2019-02-07 03:57:44 +0530

    Added Page 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './tutorialPages/Page1/Page1';
+import right_arrow from './assets/icons/right-arrow.png';
+import left_arrow from './assets/icons/left-arrow.png';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      page: 1
+    };
+  }
+
+  goBack = () => {
+    const {page} = this.state;
+    this.setState({
+      page: page - 1
+    });
+  }
+
+  goForward = () => {
+    const {page} = this.state;
+    this.setState({
+      page: page + 1
+    });
   }
 
   render() {
@@ -15,7 +34,15 @@ class App extends Component {
           <div><img src={logo} className="App-logo" alt="logo" /></div>
           <div className="Nav-bar-text">Play With React Dev Tools</div>
         </div>
-        <header className="App-header" />
+        <header className="App-header">
+        <div>
+          <button onClick={this.goBack} className="App-nav-button"><img src={left_arrow} alt="back" /></button>
+        </div>
+          <Page1 />
+          <div>
+          <button onClick={this.goForward} className="App-nav-button"><img src={right_arrow} alt="back" /></button>
+        </div>
+        </header>
       </div>
     );
   }
---
+++ src/tutorialPages/Page1/Page1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Page1.css';
+
+const Page1 = () => (
+  <div>
+    <p>Install <span className="Page1-text">(...if you haven't already)</span></p>
+    <p><a href="https://github.com/facebook/react-devtools" target="_blank" rel="noopener noreferrer">React Dev Tools</a></p>
+  </div>
+);
+
+export default Page1;
